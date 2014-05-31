a85145630c14905da6cc27de63aafdd4e8ea5486
Linus Unnebäck <linus@folkdatorn.se>
2014-05-31 16:41:35 +0200

    test: paths with special characters

--- test/xattr.js
+++ test/xattr.js
@@ -110,3 +110,57 @@ describe('xattr#async', function () {
   });
 
 });
+
+describe('xattr#utf8', function () {
+
+  var path;
+
+  before(function (done) {
+    tmp.file({ prefix: '∞ ' }, function (err, _path, fd) {
+      if (err) { return done(err); }
+
+      path = _path;
+      fs.close(fd, done);
+    });
+  });
+
+  it('should set an attribute', function (done) {
+    xattr.set(path, attribute, payload, done);
+  });
+
+  it('should get an attribute', function (done) {
+    xattr.get(path, attribute, function (err, val) {
+      if (err) { throw err; }
+      assert(Buffer.isBuffer(val));
+      assert.equal(val, payload);
+      done();
+    });
+  });
+
+  it('should list the attributes', function (done) {
+    xattr.list(path, function (err, list) {
+      if (err) { throw err; }
+      assert.ok(~list.indexOf(attribute));
+      done();
+    });
+  });
+
+  it('should remove the attribute', function (done) {
+    xattr.remove(path, attribute, done);
+  });
+
+  it('should give useful errors', function (done) {
+    xattr.get(path, attribute, function (err, val) {
+      assert(err);
+      assert.equal(err.errno, 93);
+      assert.equal(err.code, "ENOATTR");
+      assert.equal(val, undefined);
+      done();
+    });
+  });
+
+  after(function (done) {
+    fs.unlink(path, done);
+  });
+
+});
