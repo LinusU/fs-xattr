6aca37a537a29815671e9b135af50f906683da90
Linus Unnebäck <linus@folkdatorn.se>
2015-08-01 20:07:07 +0200

    coding style: standard

--- index.js
+++ index.js
@@ -1,108 +1,100 @@
+var addon = require('./build/Release/xattr')
 
-var addon = require('./build/Release/xattr');
-
-var fakeAsync = function (fn, args, cb) {
+function fakeAsync (fn, args, cb) {
   process.nextTick(function () {
     try {
-      cb(null, addon[fn].apply(addon, args));
+      cb(null, addon[fn].apply(addon, args))
     } catch (err) {
-      cb(err);
+      cb(err)
     }
-  });
-};
+  })
+}
 
-var validateArgument = function (key, val) {
+function defaultCallback (err) {
+  if (err) throw err
+}
+
+function validateArgument (key, val) {
   switch (key) {
     case 'path':
-      if (typeof val !== 'string') {
-        throw new TypeError('`' + key + '` must be a string');
-      }
-      return new Buffer(val).toString('binary');
+      if (typeof val === 'string') return new Buffer(val).toString('binary')
+      throw new TypeError('`path` must be a string')
     case 'attr':
-      if (typeof val !== 'string') {
-        throw new TypeError('`' + key + '` must be a string');
-      }
-      return val;
+      if (typeof val === 'string') return val
+      throw new TypeError('`attr` must be a string')
     case 'value':
-      if (typeof val === 'string') {
-        return new Buffer(val);
-      }
-
-      if (!Buffer.isBuffer(val)) {
-        throw new TypeError('`value` must be a string or buffer');
-      }
-
-      return val;
+      if (typeof val === 'string') return new Buffer(val)
+      if (Buffer.isBuffer(val)) return val
+      throw new TypeError('`value` must be a string or buffer')
     case 'cb':
-      if (typeof val !== 'function') {
-        return function (err) { if (err) { throw err; } };
-      }
-      return val;
+      if (typeof val === 'function') return val
+      if (val == null) return defaultCallback
+      throw new TypeError('`cb` must be a function')
     default:
-      throw new Error('Unknown argument: ' + key);
+      throw new Error('Unknown argument: ' + key)
   }
-};
+}
 
 /* Async methods */
 
-exports.get = function (path, attr, cb) {
-  path = validateArgument('path', path);
-  attr = validateArgument('attr', attr);
-  cb = validateArgument('cb', cb);
+exports.get = function get (path, attr, cb) {
+  path = validateArgument('path', path)
+  attr = validateArgument('attr', attr)
+  cb = validateArgument('cb', cb)
 
-  fakeAsync('get', [path, attr], cb);
-};
+  fakeAsync('get', [path, attr], cb)
+}
 
-exports.set = function (path, attr, value, cb) {
-  path = validateArgument('path', path);
-  attr = validateArgument('attr', attr);
-  value = validateArgument('value', value);
-  cb = validateArgument('cb', cb);
+exports.set = function set (path, attr, value, cb) {
+  path = validateArgument('path', path)
+  attr = validateArgument('attr', attr)
+  value = validateArgument('value', value)
+  cb = validateArgument('cb', cb)
 
-  fakeAsync('set', [path, attr, value], cb);
-};
+  fakeAsync('set', [path, attr, value], cb)
+}
 
-exports.list = function (path, cb) {
-  path = validateArgument('path', path);
-  cb = validateArgument('cb', cb);
+exports.list = function list (path, cb) {
+  path = validateArgument('path', path)
+  cb = validateArgument('cb', cb)
 
-  fakeAsync('list', [path], cb);
-};
+  fakeAsync('list', [path], cb)
+}
 
-exports.remove = function (path, attr, cb) {
-  path = validateArgument('path', path);
-  attr = validateArgument('attr', attr);
-  cb = validateArgument('cb', cb);
+exports.remove = function remove (path, attr, cb) {
+  path = validateArgument('path', path)
+  attr = validateArgument('attr', attr)
+  cb = validateArgument('cb', cb)
 
-  fakeAsync('remove', [path, attr], cb);
-};
+  fakeAsync('remove', [path, attr], cb)
+}
 
 /* Sync methods */
 
-exports.getSync = function (path, attr) {
-  path = validateArgument('path', path);
-  attr = validateArgument('attr', attr);
+exports.getSync = function getSync (path, attr) {
+  path = validateArgument('path', path)
+  attr = validateArgument('attr', attr)
 
-  return addon.get(path, attr);
-};
+  return addon.get(path, attr)
+}
 
-exports.setSync = function (path, attr, value) {
-  path = validateArgument('path', path);
-  attr = validateArgument('attr', attr);
-  value = validateArgument('value', value);
+exports.setSync = function setSync (path, attr, value) {
+  path = validateArgument('path', path)
+  attr = validateArgument('attr', attr)
+  value = validateArgument('value', value)
 
-  return addon.set(path, attr, value);
-};
+  return addon.set(path, attr, value)
+}
 
-exports.listSync = function (path) {
-  path = validateArgument('path', path);
+exports.listSync = function listSync (path) {
+  path = validateArgument('path', path)
 
-  return addon.list(path);
-};
+  return addon.list(path)
+}
 
-exports.removeSync = function (path, attr) {
-  path = validateArgument('path', path);
-  attr = validateArgument('attr', attr);
+exports.removeSync = function removeSync (path, attr) {
+  path = validateArgument('path', path)
+  attr = validateArgument('attr', attr)
 
-  return addon.remove(path, attr);
-};
+  return addon.remove(path, attr)
+}
--- test/xattr.js
+++ test/xattr.js
@@ -1,151 +1,152 @@
+/* eslint-env mocha */
 
-var xattr = require('../');
+var xattr = require('../')
 
-var fs = require('fs');
-var temp = require('fs-temp');
-var assert = require('assert');
-var crypto = require('crypto');
+var fs = require('fs')
+var temp = require('fs-temp')
+var assert = require('assert')
+var crypto = require('crypto')
 
-var attribute = 'com.linusu.test';
-var payload = crypto.randomBytes(24).toString('hex');
+var attribute = 'com.linusu.test'
+var payload = crypto.randomBytes(24).toString('hex')
 
 describe('xattr#sync', function () {
 
-  var path;
+  var path
 
   before(function () {
-    path = temp.writeFileSync('');
-  });
+    path = temp.writeFileSync('')
+  })
 
   it('should set an attribute', function () {
-    xattr.setSync(path, attribute, payload);
-  });
+    xattr.setSync(path, attribute, payload)
+  })
 
   it('should get an attribute', function () {
-    var val = xattr.getSync(path, attribute);
-    assert(Buffer.isBuffer(val));
-    assert.equal(val, payload);
-  });
+    var val = xattr.getSync(path, attribute)
+    assert(Buffer.isBuffer(val))
+    assert.equal(val, payload)
+  })
 
   it('should list the attributes', function () {
-    var val = xattr.listSync(path);
-    assert.ok(~val.indexOf(attribute));
-  });
+    var val = xattr.listSync(path)
+    assert.ok(~val.indexOf(attribute))
+  })
 
   it('should remove the attribute', function () {
-    xattr.removeSync(path, attribute);
-  });
+    xattr.removeSync(path, attribute)
+  })
 
   it('should give useful errors', function () {
     assert.throws(function () {
-      xattr.getSync(path, attribute);
+      xattr.getSync(path, attribute)
     }, function (err) {
-      assert.equal(err.errno, 93);
-      assert.equal(err.code, "ENOATTR");
-      return true;
-    });
-  });
+      assert.equal(err.errno, 93)
+      assert.equal(err.code, 'ENOATTR')
+      return true
+    })
+  })
 
   after(function (done) {
-    fs.unlink(path, done);
-  });
+    fs.unlink(path, done)
+  })
 
-});
+})
 
 describe('xattr#async', function () {
 
-  var path;
+  var path
 
   before(function () {
-    path = temp.writeFileSync('');
-  });
+    path = temp.writeFileSync('')
+  })
 
   it('should set an attribute', function (done) {
-    xattr.set(path, attribute, payload, done);
-  });
+    xattr.set(path, attribute, payload, done)
+  })
 
   it('should get an attribute', function (done) {
     xattr.get(path, attribute, function (err, val) {
-      if (err) { throw err; }
-      assert(Buffer.isBuffer(val));
-      assert.equal(val, payload);
-      done();
-    });
-  });
+      assert.ifError(err)
+      assert(Buffer.isBuffer(val))
+      assert.equal(val, payload)
+      done()
+    })
+  })
 
   it('should list the attributes', function (done) {
     xattr.list(path, function (err, list) {
-      if (err) { throw err; }
-      assert.ok(~list.indexOf(attribute));
-      done();
-    });
-  });
+      assert.ifError(err)
+      assert.ok(~list.indexOf(attribute))
+      done()
+    })
+  })
 
   it('should remove the attribute', function (done) {
-    xattr.remove(path, attribute, done);
-  });
+    xattr.remove(path, attribute, done)
+  })
 
   it('should give useful errors', function (done) {
     xattr.get(path, attribute, function (err, val) {
-      assert(err);
-      assert.equal(err.errno, 93);
-      assert.equal(err.code, "ENOATTR");
-      assert.equal(val, undefined);
-      done();
-    });
-  });
+      assert(err)
+      assert.equal(err.errno, 93)
+      assert.equal(err.code, 'ENOATTR')
+      assert.equal(val, undefined)
+      done()
+    })
+  })
 
   after(function (done) {
-    fs.unlink(path, done);
-  });
+    fs.unlink(path, done)
+  })
 
-});
+})
 
 describe('xattr#utf8', function () {
 
-  var path;
+  var path
 
   before(function () {
-    path = temp.template('∞ %s').writeFileSync('');
-  });
+    path = temp.template('∞ %s').writeFileSync('')
+  })
 
   it('should set an attribute', function (done) {
-    xattr.set(path, attribute, payload, done);
-  });
+    xattr.set(path, attribute, payload, done)
+  })
 
   it('should get an attribute', function (done) {
     xattr.get(path, attribute, function (err, val) {
-      if (err) { throw err; }
-      assert(Buffer.isBuffer(val));
-      assert.equal(val, payload);
-      done();
-    });
-  });
+      assert.ifError(err)
+      assert(Buffer.isBuffer(val))
+      assert.equal(val, payload)
+      done()
+    })
+  })
 
   it('should list the attributes', function (done) {
     xattr.list(path, function (err, list) {
-      if (err) { throw err; }
-      assert.ok(~list.indexOf(attribute));
-      done();
-    });
-  });
+      assert.ifError(err)
+      assert.ok(~list.indexOf(attribute))
+      done()
+    })
+  })
 
   it('should remove the attribute', function (done) {
-    xattr.remove(path, attribute, done);
-  });
+    xattr.remove(path, attribute, done)
+  })
 
   it('should give useful errors', function (done) {
     xattr.get(path, attribute, function (err, val) {
-      assert(err);
-      assert.equal(err.errno, 93);
-      assert.equal(err.code, "ENOATTR");
-      assert.equal(val, undefined);
-      done();
-    });
-  });
+      assert(err)
+      assert.equal(err.errno, 93)
+      assert.equal(err.code, 'ENOATTR')
+      assert.equal(val, undefined)
+      done()
+    })
+  })
 
   after(function (done) {
-    fs.unlink(path, done);
-  });
+    fs.unlink(path, done)
+  })
 
-});
+})
